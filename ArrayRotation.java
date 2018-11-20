650c4e1d238dd1b3024222dd67789f2e3b89a285
gopinathboosam <gopee1329@gmail.com>
2018-11-20 19:49:03 +0530

    Included Javadoc changes

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+/**
+*@author gopi
+* Rotating an array
+*/
 public class ArrayRotation {
 public static void main(String[] args) {
 	int[] intt= {1,2,3,4,5};
